8c11d9a536bde314e04c46f700531604d8760899
vishalgour2000 <52736655+vishalgour2000@users.noreply.github.com>
2021-08-21 20:29:37 +0530

    Delete twitter_credentials.py

--- twitter_credentials.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-# Variables that contains the user credentials to access Twitter API 
-ACCESS_TOKEN = "1269660043793719301-I9OlrIPycjYSfye4a3wsq9nyiNenEL"
-ACCESS_TOKEN_SECRET = "BaalVvFxL1HFuNtjpI3NjBv0crtn8hIkaiX4KaruaRTCB"
-CONSUMER_KEY = "4xi5Sldelz7eARVI4eVfZ5sKm"
-CONSUMER_SECRET = "DTx8sCjBVNkqbWkShE7Ci6YPjjoswHzrmGlC0pX9fzG0TjjyJH"
-#please do change the value of the variables after you have created a twitter app copy your own credentials to the variables 
